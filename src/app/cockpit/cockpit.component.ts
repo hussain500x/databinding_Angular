90fae26b1c51784563733809adcc79276681e8e2
hussain500x <82269242+hussain500x@users.noreply.github.com>
2023-02-05 12:19:27 +0300

    Using Local References in Templates

--- src/app/cockpit/cockpit.component.ts
+++ src/app/cockpit/cockpit.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit,EventEmitter, Output, } from '@angular/core';
+import { Component, OnInit,EventEmitter, Output, ViewChild} from '@angular/core';
 
 @Component({
   selector: 'app-cockpit',
@@ -10,27 +10,50 @@ export class CockpitComponent implements OnInit {
   @Output() bluePrintCreated = new EventEmitter<{serverName: string, serverContent:string }>();
   @Output() serverCreated = new EventEmitter<{serverName:string, serverContent:string}>();
 
-
-  serverElements = [];
   newServerName = '';
   newServerContent = '';
 
+//  @ViewChild('serverContentInput', {static: true}) serverContentInput: ElementRef;
   constructor() { }
   ngOnInit(): void {
   }
   
-  onAddServer() {
+  // onAddServer() {
+  //   this.serverCreated.emit({
+  //     serverName: this.newServerName,
+  //     serverContent: this.newServerContent
+  //   });
+  // }
+
+//   onAddBlueprint() {
+//     this.bluePrintCreated.emit({
+//       serverName: this.newServerName,
+//       serverContent: this.newServerContent
+//     });
+//   }
+
+
+
+
+
+//// Othwe Way To Print /////
+
+  onAddServer(nameInput: HTMLInputElement) {
+    // console.log(nameInput.value);
     this.serverCreated.emit({
-      serverName: this.newServerName,
+      serverName:nameInput.value,
       serverContent: this.newServerContent
     });
   }
 
-  onAddBlueprint() {
+  
+  onAddBlueprint(nameInput: HTMLInputElement) {
     this.bluePrintCreated.emit({
-      serverName: this.newServerName,
+      serverName:nameInput.value,
       serverContent: this.newServerContent
     });
   }
+
+
 
 }
